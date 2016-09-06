035bac03650dd1d33a967bcc11c595a07b79cb3d
KYIV\Petro_Vozhdai <Petro_Vozhdai@epam.com>
2016-09-06 12:53:06 +0300

    add tests

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TransferFunds() {
+           
+            Assert.AreEqual( 250m, 250m);
+        }
+
+    }
+}
